c8487f79c25ca01ea9992e9b8c02f8bf34a27b10
Nanotect <61177761+Adivise@users.noreply.github.com>
2022-01-28 10:27:49 +0700

    Update Premium.js

--- settings/models/Premium.js
+++ settings/models/Premium.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const CreatePremium = mongoose.Schema({
     member: String,
     premium: Boolean,
+    created: Number,
 });
 
 module.exports = mongoose.model('Premium', CreatePremium);
