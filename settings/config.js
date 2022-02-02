5a95d4bb683944e76c216f03d459f25757f5ac55
Shabbir Ezzy <76464869+Shabbir260505@users.noreply.github.com>
2022-02-02 11:23:15 +0530

    Update config.js

--- settings/config.js
+++ settings/config.js
@@ -4,7 +4,7 @@ module.exports = {
     TOKEN: process.env.TOKEN || "YOUR_TOKEN",  // your bot token
     PREFIX: process.env.PREFIX || "#", //<= default is #  // bot prefix
 
-    OWNER_ID: process.env.OWNER_ID || "YOUR_CLIENT_ID", //your client id
+    OWNER_ID: process.env.OWNER_ID || "YOUR_OWNER_ID", //your owner id
 
     NP_REALTIME: process.env.NP_REALTIME || "BOOLEAN", // "true" = realtime, "false" = not realtime :3 // WARNING: on set to "true" = laggy
 
